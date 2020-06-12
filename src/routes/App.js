8c1eb010aebecb9618e387a2d166416986a76a37
daniel hizo un comentario <daniellopezj0327@gmail.com>
2020-06-11 19:05:32 -0500

    create router login

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { BrowserRouter,Route} from 'react-router-dom';
-import Home from '../containers/Home'
-const App=()=>(
-    <BrowserRouter>
-    <Route exact path='/' component={Home}/>
-    </BrowserRouter>
-)
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "../containers/Home";
+import Login from "../containers/Login";
+const App = () => (
+  <BrowserRouter>
+    <Route exact path="/" component={Home} />{" "}
+    <Route exact path="/Login" component={Login} />{" "}
+  </BrowserRouter>
+);
 
 export default App;
